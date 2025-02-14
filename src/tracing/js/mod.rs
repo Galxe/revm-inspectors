a8e16ea9dc5352386b797fea1cb29ae8a17a530d
nekomoto911 <nekomoto911@gmail.com>
2025-02-15 00:57:36 +0800

    fix JsInspector

--- src/tracing/js/mod.rs
+++ src/tracing/js/mod.rs
@@ -230,7 +230,7 @@ impl JsInspector {
         DB: DatabaseRef,
         <DB as DatabaseRef>::Error: std::fmt::Display,
     {
-        let ResultAndState { result, state } = res;
+        let ResultAndState { result, state, .. } = res;
         let (db, _db_guard) = EvmDbRef::new(&state, db);
 
         let gas_used = result.gas_used();
